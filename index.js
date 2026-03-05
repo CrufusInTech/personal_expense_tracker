5bfc3bde6bcbe815f4b60ca11b5df83d745202c2
Crufus <chigozirimrufus@gmail.com>
2026-03-05 19:46:29 +0100

    View Expenses- I have finished the logic for the View expenses

--- index.js
+++ index.js
@@ -12,19 +12,50 @@ async function mainMenu(){
     ])
 
     if (choice.action === 'Add Expense') {
-    // 1. Ask for name and amount
-    // 2. Push to expenses array
-    // 3. Call mainMenu() again
+         const detailsOfExpense = await inquirer.prompt([
+            {
+                name: 'name',
+                message: 'What did you buy?',
+            },
+            {
+                name: 'amount',
+                message: 'How much did it cost?',
+            }
+        ]);
+
+        // Convert string to a float number
+        const numericAmount = parseFloat(detailsOfExpense.amount);
+        
+        // Add to the global array
+        expenses.push({name:detailsOfExpense.name, amount:numericAmount })
+
+        console.log('Expense added successfully!');
+    
+        // CRITICAL: Call the menu again to keep the app alive
+        await mainMenu();
+    
     } 
     else if (choice.action === 'View Expenses') {
-        // 1. Loop through expenses and console.log them
-        // 2. Call mainMenu() again
+        console.log('\n--- Your Expenses ---');
+
+        if(expenses === 0){
+            console.log("No expenses recorded yet");
+        }
+        else {
+            expenses.forEach((item, index) => {
+                console.log(`${index + 1}. ${item.name}: ₦${item.amount.toFixed(2)}`);
+            });
+        }
+
+        console.log('---------------------\n');
+
+        // Bring back the menu!
+        await mainMenu();
     } 
     else if (choice.action === 'Show Total') {
         // 1. Calculate total
         // 2. Call mainMenu() again
-    } 
-    else {
+    } else {
         console.log('Goodbye, Crufus!');
         process.exit();
     }
